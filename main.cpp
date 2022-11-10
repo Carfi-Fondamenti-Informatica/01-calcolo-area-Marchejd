974355fd17fee3df5476ab91ee20ca1a4b3e5b2a
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-10 16:33:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
+
 using namespace std;
 
 int main(){
 
-float a=0, b=0, c=0, S=0;
-cout<<"inseriere i valori di a,b,c :<<endl;
-cin >> a >> b >> c;
+    float a=0, b=0, c=0, S=0;
 
-S=a*b/2;
-cout<<"l'area del triangolo è : "<<S<<endl;
-S=0;
-  
-S=a*a;
-cout<<"l'area del quadrato è : "<<S<<endl;
-S=0;
-  
-S=a*b;
-cout<<"l'area del rettangolo è : "<<S<<endl;
-S=0;
-  
-S=(a+b)*c/2;
-cout<<"l'area del trapezio è : "<<S<<endl;
+    cin >> a >> b >> c;
 
+
+    S=a*b/2;
+    cout<<"l'area del triangolo è : "<<S<<endl;
+    S=0;
+    S=a*a;
+    cout<<"l'area del quadrato è : "<<S<<endl;
+    S=0;
+    S=a*b;
+    cout<<"l'area del rettangolo è : "<<S<<endl;
+    S=0;
+    S=(a+b)*c/2;
+    cout<<"l'area del trapezio è : "<<S<<endl;
+    return 0;
 }
