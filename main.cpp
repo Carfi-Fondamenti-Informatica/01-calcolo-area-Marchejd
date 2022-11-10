e81dae948378b57587586dfd8967634e8d4518a0
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-10 16:23:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
 
+float a=0, b=0, c=0, S=0;
+cout<<"inseriere i valori di a,b,c :<<endl;
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
+S=a*b/2;
+cout<<"l'area del triangolo è : "<<S<<endl;
+S=0;
+  
+S=a*a;
+cout<<"l'area del quadrato è : "<<S<<endl;
+S=0;
+  
+S=a*b;
+cout<<"l'area del rettangolo è : "<<S<<endl;
+S=0;
+  
+S=(a+b)*c/2;
+cout<<"l'area del trapezio è : "<<S<<endl;
 
 }
