0969959f1ac9c5d7ad0631f3ce642bd6cdf10658
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-10 16:39:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-#include <iostream>
+<iostream>
 
 using namespace std;
 
-int main(){
+intmain(){
 
-    float a=0, b=0, c=0, S=0;
+    floata=0, b=0, c=0, S=0;
 
     cin >> a >> b >> c;
 
 
     S=a*b/2;
-    cout<<"l'area del triangolo è : "<<S<<endl;
+    cout<< "l'area del triangolo è : " <<S<<endl;
     S=0;
+    
     S=a*a;
-    cout<<"l'area del quadrato è : "<<S<<endl;
+    cout<< "l'area del quadrato è : " <<S<<endl;
     S=0;
+    
     S=a*b;
-    cout<<"l'area del rettangolo è : "<<S<<endl;
+    cout<< "l'area del rettangolo è : " <<S<<endl;
     S=0;
+    
     S=(a+b)*c/2;
-    cout<<"l'area del trapezio è : "<<S<<endl;
+    cout<< "l'area del trapezio è : " <<S<<endl;
+    
     return 0;
 }
